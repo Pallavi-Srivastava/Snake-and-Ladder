e90add95c6478cbd45fb22b9682633ba41c74482
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-22 22:21:57 +0530

    Added:Game Played With Single Player

---
+++ SnakeNLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelab.program;
+
+public class SnakeNLadder {
+
+	// position of the player
+	static int playerPosition = 0;
+
+	public static void main(String[] args) {
+		System.out.println("------GameStart------------");
+	}
+
+}
